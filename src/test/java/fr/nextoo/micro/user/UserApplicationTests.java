ce9dfa397f50822dbd22e752529177c697c07170
Kevin Messiaen <kmessiaen@nextoo.fr>
2019-12-27 10:33:10 +0100

    init commi

---
+++ src/test/java/fr/nextoo/micro/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.nextoo.micro.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
