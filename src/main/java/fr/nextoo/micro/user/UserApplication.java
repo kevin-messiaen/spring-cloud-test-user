13c4212f6a7f0397c74e63e98299f9c2aae4b0b1
Kevin Messiaen <kmessiaen@nextoo.fr>
2019-12-27 11:40:22 +0100

    enable eureka client

--- src/main/java/fr/nextoo/micro/user/UserApplication.java
+++ src/main/java/fr/nextoo/micro/user/UserApplication.java
@@ -2,8 +2,10 @@ package fr.nextoo.micro.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserApplication {
 
 	public static void main(String[] args) {
