ba24e1b8c2028681745b6c03c3420a6d6d23779d
Kevin Messiaen <kmessiaen@nextoo.fr>
2019-12-30 11:03:47 +0100

    enable feign clients

--- src/main/java/fr/nextoo/micro/user/UserApplication.java
+++ src/main/java/fr/nextoo/micro/user/UserApplication.java
@@ -3,9 +3,11 @@ package fr.nextoo.micro.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class UserApplication {
 
 	public static void main(String[] args) {
